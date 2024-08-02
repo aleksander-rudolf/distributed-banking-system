bd53b50c42276fcd3d82bb5e91a64c0d4bd29146
Aleksander Rudolf <95549699+aleksander-rudolf@users.noreply.github.com>
2024-08-02 09:12:08 -0600

    Project upload

---
+++ server_i/src/Errors/DestinationError.java
@@ -0,0 +1,5 @@
+package Errors;
+// -4
+public class DestinationError extends Exception {
+    public DestinationError() {}
+}
---
+++ server_i/src/server_i/Lock.java
@@ -0,0 +1,25 @@
+package server_i;
+
+public class Lock {
+    //#region Class Variables
+    private String type;
+    //#endregion Class Variables
+
+    //#region Constructors
+    /**
+     * Constructor
+     * @param type The type of lock.
+     */
+    public Lock(String type) {
+        this.type = type;
+    }
+    //#endregion Constructors
+
+    //#region Getters
+    /**
+     * Get the type of lock.
+     * @return String The type of lock.
+     */
+    public String getType() { return type; }
+    //#endregion Getters
+}
